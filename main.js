1520195caddb6c4902adc83d1e9ec3e8f9bbcf03
Osaze Shears <yoze15@gmail.com>
2014-05-04 09:17:21 -0400

    Updates in movement

--- main.js
+++ main.js
@@ -34,46 +34,61 @@ function create() {
 
 function update() {
 
+    sprite.body.velocity.x=0;
+    sprite.body.velocity.y=0;
 
     if (upKey.isDown && leftKey.isDown){
-        sprite.x-=5;
-        sprite.y-=5;
+        sprite.body.velocity.x=-150;
+        sprite.body.velocity.y=-150;
         sprite.angle=135;
     }
     else if (upKey.isDown && rightKey.isDown){
-        sprite.x+=5;
-        sprite.y-=5;
+        sprite.body.velocity.x=150;
+        sprite.body.velocity.y=-150;
         sprite.angle=225;
     }
     else if (upKey.isDown)
     {
-        sprite.y-=5;
+        sprite.body.velocity.y=-150;
         sprite.angle=180;
     }
     else if (downKey.isDown && leftKey.isDown){
-        sprite.x-=5;
-        sprite.y+=5;
+        sprite.body.velocity.x=-150;
+        sprite.body.velocity.y=150;
         sprite.angle=45;
     }
     else if (downKey.isDown && rightKey.isDown){
-        sprite.x+=5;
-        sprite.y+=5;
+        sprite.body.velocity.x=150;
+        sprite.body.velocity.y=150;
         sprite.angle=315;
     }
     else if (downKey.isDown)
     {
-        sprite.y+=5;
+        sprite.body.velocity.y=150;
         sprite.angle=0;
     }
     else if (leftKey.isDown)
     {
-        sprite.x-=5;
+        sprite.body.velocity.x=-150;
         sprite.angle=90;
     }
     else if (rightKey.isDown)
     {
-        sprite.x+=5;
+        sprite.body.velocity.x=150;
         sprite.angle=270;
     }
-
+    /*
+        if(keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            player.acceleration.x += 100;
+            player.flipped = false;
+        } else if(keyboard.justReleased(Phaser.Keyboard.RIGHT)) {
+            player.acceleration.x = 0;
+        }
+        if(keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            player.acceleration.x -= 30;
+            player.flipped = true;
+        } else if(keyboard.justReleased(Phaser.Keyboard.LEFT)) {
+            player.acceleration.x = 0;
+        }
+        */
 }
